0ca2f7fb3ea87f2362f101ca7ba4c86ab74909ab
Breno Machado <bamachadu@gmail.com>
2022-03-29 13:51:14 -0300

    Testando a primera classe de dominio - Categoria

--- src/main/java/com/bamachadu/curso/resource/CategoriaResource.java
+++ src/main/java/com/bamachadu/curso/resource/CategoriaResource.java
@@ -1,5 +1,10 @@
 package com.bamachadu.curso.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bamachadu.curso.domain.Categoria;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
   @RequestMapping(method = RequestMethod.GET)
-  public String listar() {
-    return "Rest está funcionando!";
+  public List<Categoria> listar() {
+    Categoria categoria1 = new Categoria(1, "Informática");
+    Categoria categoria2 = new Categoria(1, "Escritorio");
+
+    List<Categoria> lista = new ArrayList<>();
+
+    lista.add(categoria1);
+    lista.add(categoria2);
+
+    return lista;
   }
 }
