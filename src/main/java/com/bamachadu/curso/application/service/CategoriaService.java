50ea139537ed8570fe8bb3c6ca6dc1f7b16b5e8c
Breno Machado <bamachadu@gmail.com>
2022-04-28 20:44:51 -0300

    criando um post aula 34

--- src/main/java/com/bamachadu/curso/application/service/CategoriaService.java
+++ src/main/java/com/bamachadu/curso/application/service/CategoriaService.java
@@ -18,6 +18,11 @@ public class CategoriaService {
   public Categoria findById(Integer id) {
     Optional<Categoria> obj = repository.findById(id);
     return obj.orElseThrow(() -> new ObjectNotFoundException(
-      "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+        "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+  }
+
+  public Categoria add(Categoria obj) {
+    obj.setId(null);
+    return repository.save(obj);
   }
 }
--- src/main/java/com/bamachadu/curso/presentation/controllers/CategoriaController.java
+++ src/main/java/com/bamachadu/curso/presentation/controllers/CategoriaController.java
@@ -1,14 +1,18 @@
 package com.bamachadu.curso.presentation.controllers;
 
+import java.net.URI;
+
 import com.bamachadu.curso.application.service.CategoriaService;
 import com.bamachadu.curso.entity.domain.Categoria;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -22,4 +26,13 @@ public class CategoriaController {
     Categoria obj = service.findById(id);
     return ResponseEntity.ok().body(obj);
   }
+
+  @RequestMapping(method = RequestMethod.POST)
+  public ResponseEntity<Void> add(@RequestBody Categoria obj) {
+    obj = service.add(obj);
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+    return ResponseEntity.created(uri).build();
+  }
+
 }
